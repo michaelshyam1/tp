334d4b93aed07e664a211ade2d1948834501d9cf
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-11 20:40:07 +0800

    New calender class

---
+++ src/main/java/seedu/duke/calender/Calendar.java
@@ -0,0 +1,39 @@
+package seedu.duke.calender;
+
+import seedu.duke.task.Deadline;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+public class Calendar {
+    private TreeMap<LocalDate, List<Deadline>> schedule;
+
+    public Calendar() {
+        this.schedule = new TreeMap<>();
+    }
+
+    public void registerDeadline(Deadline deadline) {
+        LocalDate date = deadline.getBy().toLocalDate();
+        schedule.computeIfAbsent(date, k -> new ArrayList<>()).add(deadline);
+    }
+
+    public void clear() {
+        schedule.clear();
+    }
+
+    public void displayRange(LocalDate start, LocalDate end) {
+        var view = schedule.subMap(start, true, end, true);
+        if (view.isEmpty()) {
+            System.out.println("No deadlines found in this range.");
+            return;
+        }
+
+        for (var entry : view.entrySet()) {
+            System.out.println("--- " + entry.getKey() + " ---");
+            for (Deadline d : entry.getValue()) {
+                System.out.println(d);
+            }
+        }
+    }
+}
