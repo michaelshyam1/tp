2e4bed0203260c01899503ea18757499b1dd7aea
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-21 21:02:22 +0800

    New EventUi class

---
+++ src/main/java/seedu/duke/ui/EventUi.java
@@ -0,0 +1,22 @@
+package seedu.duke.ui;
+
+import seedu.duke.task.Event;
+
+public class EventUi {
+
+    public static void printEventAdded(Event event, String categoryName, int count) {
+        GeneralUi.printDottedLine();
+        System.out.println(" Got it. I've added this event to category: " + categoryName);
+        System.out.println("   " + event);
+        System.out.println(" Now you have " + count + " events in this category.");
+        GeneralUi.printDottedLine();
+    }
+
+    public static void printRecurringEventAdded(Event event) {
+        GeneralUi.printWithBorder("This recurring event has been added:", event.toStringRecurring());
+    }
+
+    public static void printRecurringEventDeleted(Event event) {
+        GeneralUi.printWithBorder("This recurring event has been deleted:", event.toStringRecurringList());
+    }
+}
